cac0d4701581c46465810ae7f9dbdc9063f9b045
MatheusOPP <54760569+MatheusOPP@users.noreply.github.com>
2022-06-19 11:13:09 -0300

    Add module with function for directory setup

---
+++ directory_setup.py
@@ -0,0 +1,7 @@
+from settings import *
+
+def directory_setup():
+    '''Set up directories if they don't exist'''
+    Path(IMAGE_OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
+    for gender in GENDER_LIST:
+        (Path(IMAGE_OUTPUT_FOLDER) / gender).mkdir(parents=True, exist_ok=True)
