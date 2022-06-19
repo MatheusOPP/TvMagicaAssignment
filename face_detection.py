cf730d223d43b1c512fcace0c423ce142db1b94d
MatheusOPP <54760569+MatheusOPP@users.noreply.github.com>
2022-06-19 11:13:24 -0300

    Add module for face detection

---
+++ face_detection.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+from settings import *
+
+
+def get_faces(frame, face_net, confidence_threshold=0.5):
+    # convert the frame into a blob to be ready for NN input
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 177.0, 123.0))
+    # set the image as input to the NN
+    face_net.setInput(blob)
+    # perform inference and get predictions
+    output = np.squeeze(face_net.forward())
+    # initialize the result list
+    faces = []
+    # Loop over the faces detected
+    for i in range(output.shape[0]):
+        confidence = output[i, 2]
+        if confidence > confidence_threshold:
+            box = output[i, 3:7] * np.array(
+                [frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]]
+            )
+            # convert to integers
+            start_x, start_y, end_x, end_y = box.astype(np.int)
+            # widen the box a little
+            start_x, start_y, end_x, end_y = (
+                start_x - 10,
+                start_y - 10,
+                end_x + 10,
+                end_y + 10,
+            )
+            start_x = 0 if start_x < 0 else start_x
+            start_y = 0 if start_y < 0 else start_y
+            end_x = 0 if end_x < 0 else end_x
+            end_y = 0 if end_y < 0 else end_y
+            # append to our list
+            faces.append((start_x, start_y, end_x, end_y))
+    return faces
