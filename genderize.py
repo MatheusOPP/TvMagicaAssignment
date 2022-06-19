d2f492c68bff8170b892cf4ec97347120b5bea22
MatheusOPP <54760569+MatheusOPP@users.noreply.github.com>
2022-06-19 11:21:58 -0300

    Apply black formatting

--- genderize.py
+++ genderize.py
@@ -14,4 +14,5 @@ gender_net = cv2.dnn.readNetFromCaffe(GENDER_MODEL, GENDER_PROTO)
 if __name__ == "__main__":
     # Parsing command line arguments entered by user
     import sys
+
     generate_gendered_images(sys.argv[1], face_net, gender_net)
