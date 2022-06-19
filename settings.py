e2811329db6e79cac4fa239ad4acb190e7a6314c
MatheusOPP <54760569+MatheusOPP@users.noreply.github.com>
2022-06-19 11:10:36 -0300

    Add a file to set up some global constants for settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+# The gender model architecture
+# https://drive.google.com/open?id=1W_moLzMlGiELyPxWiYQJ9KFaXroQ_NFQ
+GENDER_MODEL = "weights/deploy_gender.prototxt"
+# The gender model pre-trained weights
+# https://drive.google.com/open?id=1AW3WduLk1haTVAxHOkVS_BEzel1WXQHP
+GENDER_PROTO = "weights/gender_net.caffemodel"
+# Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
+# substraction to eliminate the effect of illunination changes
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+# Represent the gender classes
+GENDER_LIST = ["masculino", "feminino"]
+# https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+FACE_PROTO = "weights/deploy.prototxt"
+# https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
+FACE_MODEL = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+
+IMAGE_OUTPUT_FOLDER = "new_dataset_img"
