a97b4855dc87c65e8507650597dbc8ed02840a55
MatheusOPP <54760569+MatheusOPP@users.noreply.github.com>
2022-06-19 11:14:38 -0300

    Add main module

---
+++ genderize.py
@@ -0,0 +1,17 @@
+# Import Libraries
+import cv2
+import numpy as np
+from predict_gender import generate_gendered_images
+from settings import *
+import uuid
+from pathlib import Path
+
+# load face Caffe model
+face_net = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
+# Load gender prediction model
+gender_net = cv2.dnn.readNetFromCaffe(GENDER_MODEL, GENDER_PROTO)
+
+if __name__ == "__main__":
+    # Parsing command line arguments entered by user
+    import sys
+    generate_gendered_images(sys.argv[1], face_net, gender_net)
